0c7ddd8c96a93b3aa70086842728d1be7507b4bd
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2017-11-07 13:22:34 -0800

    Membuat panjang, lebar,luas,pilihan,alas,tinggi

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+	int panjang, lebar, luas, pilihan, alas, tinggi;
 	
+	cout <<"1.Persegi panjang\n";
+	cout <<"2.Segitiga\n";
+	cout <<"Masukan Pilihan =";cin >>pilihan;	
 }
