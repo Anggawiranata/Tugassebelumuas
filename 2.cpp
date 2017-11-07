8dabd592dcfc3358ef03bf3df1cdc7b80d8194d3
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2017-11-07 13:34:32 -0800

    Memasukkan panjang,lebar,luas,persegi panjang

--- 2.cpp
+++ 2.cpp
@@ -6,5 +6,22 @@ int main(int argc, char** argv){
 	
 	cout <<"1.Persegi panjang\n";
 	cout <<"2.Segitiga\n";
-	cout <<"Masukan Pilihan =";cin >>pilihan;	
+	cout <<"Masukan Pilihan =";cin >>pilihan;
+	
+	switch(pilihan){
+		case 1:
+		cout<<"masukan panjang=";cin>>panjang;
+		cout<<"masukan lebar=";cin>>lebar;
+		luas =panjang * lebar;
+		cout<<"luas persegi panjang="<<luas<<"\n";
+		system("pause");
+		break;
+		case 2:
+		cout<<"masukan panjang alas segitiga=";cin>>alas;
+		cout<<"masukan tinggi segitiga=";cin>>tinggi;
+		luas=0.5*alas*tinggi;
+		cout<<"luas segitiga="<<luas<<"\n";
+		system("pause");
+	}	
 }
+
