577fbd34b9e8f2db7666f8b6c69f7f0275c7ad8f
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2017-11-07 19:30:11 -0800

    Menentukan nilai positif,negatif dan nol

--- 4.cpp
+++ 4.cpp
@@ -7,5 +7,15 @@ int main(){
 	int x;
 	cout << "Masukan Nilai Integer : ";
 	cin >> x;
+	
+	if (x>0){
+		cout << "Nilai positif";
+	}
+	else if (x<0){
+		cout << "Nilai negatif";
+	}
+	else{
+		cout << "Nilai Nol";
+	}
 
 }
